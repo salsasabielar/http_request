e014fe943d5f1b9263170e8cc1eb56a92bdd98a3
= <=>
2021-04-08 11:57:34 +0700

    praktikum http request

---
+++ lib/pages/movie_list.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:http_request/service/http_service.dart';
+
+import 'movie_detail.dart';
+
+class MovieList extends StatefulWidget {
+  @override
+  _MovieListState createState() => _MovieListState();
+}
+
+class _MovieListState extends State<MovieList> {
+  int moviesCount;
+  List movies;
+  HttpService service;
+
+  Future initialize() async {
+    movies = [];
+    movies = await service.getPopularMovies();
+    setState(() {
+      moviesCount = movies.length;
+      movies = movies;
+    });
+  }
+
+  @override
+  void initState() {
+    service = HttpService();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Popular Movies"),
+      ),
+      body: ListView.builder(
+        itemCount: (this.moviesCount == null) ? 0 : this.moviesCount,
+        itemBuilder: (context, int position) {
+          return Card(
+            color: Colors.white,
+            elevation: 2.0,
+            child: ListTile(
+              title: Text(movies[position].title),
+              subtitle: Text(
+                'Rating = ' + movies[position].voteAverage.toString(),
+              ),
+              onTap: () {
+                MaterialPageRoute route = MaterialPageRoute(
+                    builder: (_) => MovieDetail(movies[position]));
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/service/http_service.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:http_request/model/movie.dart';
+
+class HttpService {
+  final String apiKey = '5d7f4713f7afca5bbe780b02048526b3';
+  final String baseUrl = 'https://api.themoviedb.org/3/movie/550?api_key=';
+
+  Future<List> getPopularMovies() async {
+    final String uri = baseUrl + apiKey;
+
+    http.Response result = await http.get(Uri.parse(uri));
+    if (result.statusCode == HttpStatus.ok) {
+      print("Sukses");
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['result'];
+      List movies = moviesMap.map((i) => Movie.fromJson((i).toList()));
+      return movies;
+    } else {
+      print("Fail");
+      return null;
+    }
+  }
+}
